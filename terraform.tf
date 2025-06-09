93bf21655e659490acb76aefd4a8299f6b299810
tayden-b <tayden.barretto@hashicorp.com>
2025-06-09 10:18:54 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,19 +3,29 @@
 
 terraform {
 
-  /* Uncomment this block to use Terraform Cloud for this tutorial
   cloud {
-      organization = "organization-name"
+      organization = "hashi-org-TF"
       workspaces {
-        name = "learn-terraform-variables"
+        name = "hvs-terraform"
       }
   }
-  */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.42.0"
     }
+  hcp = {
+      source = "hashicorp/hcp"
+      version = "0.94.1"
+    }
   }
   required_version = "~> 1.2"
 }
+
+# Specify the app in the project (one app_name per data block)
+data "hcp_vault_secrets_app" "aws_app" {
+  app_name = "terraform"
+}
+
+# Replace your existing secret references with
+# data.hcp-vault-secrets_app.aws_app.secret-name
