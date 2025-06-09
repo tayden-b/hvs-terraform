bd5b7d3e2d130665c7f88e2929e519c8873eabfb
tayden-b <tayden.barretto@hashicorp.com>
2025-06-09 10:16:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+
+
 provider "aws" {
   region  = "us-west-2"
 }
+
 
 data "aws_availability_zones" "available" {
   state = "available"
